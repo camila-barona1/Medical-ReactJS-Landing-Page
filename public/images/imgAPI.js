9a5b662ad99ead3d78e65e7c4e65169f5e2795ea
Maria Camila <camilabarona1@gmail.com>
2025-06-30 17:11:17 -0500

    project

---
+++ public/images/imgAPI.js
@@ -0,0 +1,49 @@
+const imgAPI = {
+  avatar: [
+    '/images/avatars/pp_girl.svg',
+    '/images/avatars/pp_girl2.svg',
+    '/images/avatars/pp_girl3.svg',
+    '/images/avatars/pp_girl4.svg',
+    '/images/avatars/pp_girl5.svg',
+    '/images/avatars/pp_girl.svg',
+    '/images/avatars/pp_boy.svg',
+    '/images/avatars/pp_boy2.svg',
+    '/images/avatars/pp_boy3.svg',
+    '/images/avatars/pp_boy4.svg',
+    '/images/avatars/pp_boy5.svg'
+  ],
+  photo: [
+    'https://via.placeholder.com/675x900/e1ad92/fff',
+    'https://via.placeholder.com/967x725/ea6d6d/fff',
+    'https://via.placeholder.com/1280x849/ea6db7/fff',
+    'https://via.placeholder.com/967x725/bb6dea/fff',
+    'https://via.placeholder.com/1048x701/6d6fea/fff',
+    'https://via.placeholder.com/1050x700/6dc0ea/fff',
+    'https://via.placeholder.com/970x725/6deaa6/fff',
+    'https://via.placeholder.com/1051x700/b8de27/fff',
+    'https://via.placeholder.com/1051x701/de9f27/fff',
+    'https://via.placeholder.com/1050x700/ef4545/fff',
+    'https://via.placeholder.com/1050x700/ffc4c4/757575',
+    'https://via.placeholder.com/640x447/fdffc4/757575',
+    'https://via.placeholder.com/1280x851/c4ffd7/757575',
+    'https://via.placeholder.com/640x425/c4cdff/757575',
+  ],
+  medical: [
+    'https://images.pexels.com/photos/236380/pexels-photo-236380.jpeg',
+    'https://images.pexels.com/photos/24902845/pexels-photo-24902845.jpeg',
+    'https://images.pexels.com/photos/7563452/pexels-photo-7563452.jpeg',
+    'https://images.pexels.com/photos/24022802/pexels-photo-24022802.jpeg',
+    'https://images.pexels.com/photos/4101143/pexels-photo-4101143.jpeg',
+    'https://images.pexels.com/photos/3452997/pexels-photo-3452997.jpeg',
+    'https://images.pexels.com/photos/6754161/pexels-photo-6754161.jpeg',
+    '/images/avatars/pp_girl4.svg',
+    '/images/avatars/pp_girl5.svg',
+    '/images/avatars/pp_girl.svg',
+    '/images/avatars/pp_boy3.svg',
+    '/images/avatars/pp_boy4.svg',
+    '/images/avatars/pp_boy5.svg',
+    'https://via.placeholder.com/964x723/1071ef/FFFFFF'
+  ],
+};
+
+export default imgAPI;
